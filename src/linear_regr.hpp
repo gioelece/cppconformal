775771082e04701d3b74c997b864e82149f3e04f
Gioele Cerri <gioele.cerri@mail.polimi.it>
2020-10-27 12:11:18 +0100

    Use MatrixBase to avoid storing itermediate results

--- src/linear_regr.hpp
+++ src/linear_regr.hpp
@@ -5,8 +5,15 @@ using namespace Eigen;
 
 class LinearRegression {
     public:
-    void fit(const MatrixXd & X, const MatrixXd & y);
-    MatrixXd predict(const MatrixXd & X0);
+    template<typename Derived1, typename Derived2>
+    void fit(const MatrixBase<Derived1> & X, const MatrixBase<Derived2> & y) {
+        beta = (X.transpose() * X).ldlt().solve(X.transpose() * y);
+    }
+
+    template<typename Derived>
+    MatrixXd predict(const MatrixBase<Derived> & X0) {
+        return X0 * beta;
+    }
 
     private:
     MatrixXd beta;
@@ -15,8 +22,18 @@ class LinearRegression {
 class RidgeRegression {
     public:
     RidgeRegression(double l) : lambda(l) {};
-    void fit(const MatrixXd & X, const MatrixXd & y);
-    MatrixXd predict(const MatrixXd & X0);
+
+    template<typename Derived1, typename Derived2>
+    void fit(const MatrixBase<Derived1> & X, const MatrixBase<Derived2> & y) {
+        int p = X.cols();
+        auto eye = MatrixXd::Identity(p, p);
+        beta = (X.transpose() * X + lambda * eye).ldlt().solve(X.transpose() * y);
+    }
+
+    template<typename Derived>
+    MatrixXd predict(const MatrixBase<Derived> & X0) {
+        return X0 * beta;
+    }
 
     private:
     double lambda;

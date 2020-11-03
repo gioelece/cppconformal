85accfc7d9012146a047312d33c4dc51a1a3a52a
Gioele Cerri <gioele.cerri@mail.polimi.it>
2020-11-03 22:17:52 +0100

    Create a base class for linear regression models

--- src/linear_regr.hpp
+++ src/linear_regr.hpp
@@ -3,41 +3,70 @@
 #include <RcppEigen.h>
 using namespace Eigen;
 
-class LinearRegression {
+/*! Base class for linear regression models.
+    Provides the default copy constructor.
+*/
+class LinearRegressionBase {
     public:
-    template<typename Derived1, typename Derived2>
-    void fit(const MatrixBase<Derived1> & X, const MatrixBase<Derived2> & y) {
-        beta = (X.transpose() * X).ldlt().solve(X.transpose() * y);
-    }
-
+    /*! Use a fitted linear regression model to make a prediction
+        \param X0 matrix of independent variables
+        \returns prediction of Y corresponding to X0
+    */
     template<typename Derived>
     MatrixXd predict(const MatrixBase<Derived> & X0) {
+        if (!is_fitted) {
+            Rcpp::stop("Linear model has not been fitted yet");
+        }
         return X0 * beta;
+    }
+
+    protected:
+    void set_beta(MatrixXd new_beta) {
+        beta = new_beta;
+        is_fitted = true;
     }
 
     private:
     MatrixXd beta;
+    bool is_fitted = false;
 };
 
-class RidgeRegression {
+/*! Class holding a linear regression model.
+*/
+class LinearRegression : public LinearRegressionBase {
     public:
-    RidgeRegression(double l) : lambda(l) {};
-
+    /*! Fit the linear regression model
+        \param X matrix of independent variables
+        \param Y matrix of covariates
+    */
     template<typename Derived1, typename Derived2>
     void fit(const MatrixBase<Derived1> & X, const MatrixBase<Derived2> & y) {
+        set_beta((X.transpose() * X).ldlt().solve(X.transpose() * y));
+    }
+};
+
+/*! Class holding a ridge regression model.
+*/
+class RidgeRegression : public LinearRegressionBase {
+    public:
+    /*! Constructs a ridge regression model instance.
+        \params l lambda (if lambda = 0, equivalent to linear regression)
+    */
+    RidgeRegression(double l) : lambda(l) {};
+
+    /*! Fit the ridge regression model
+        \param X matrix of independent variables
+        \param Y matrix of covariates
+    */
+    template<typename Derived1, typename Derived2>
+    void fit(const MatrixBase<Derived1> & X, const MatrixBase<Derived2> & Y) {
         int p = X.cols();
         auto eye = MatrixXd::Identity(p, p);
-        beta = (X.transpose() * X + lambda * eye).ldlt().solve(X.transpose() * y);
-    }
-
-    template<typename Derived>
-    MatrixXd predict(const MatrixBase<Derived> & X0) {
-        return X0 * beta;
+        set_beta((X.transpose() * X + lambda * eye).ldlt().solve(X.transpose() * Y));
     }
 
     private:
     double lambda;
-    MatrixXd beta;
 };
 
 #endif
